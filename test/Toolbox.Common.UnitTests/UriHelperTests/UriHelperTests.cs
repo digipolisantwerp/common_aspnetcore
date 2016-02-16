5e145733328a0072c5d713a5451565e52971daf7
kstr <info@elkit.be>
2016-02-16 16:01:44 +0100

    Added class + tests + translation

---
+++ test/Toolbox.Common.UnitTests/UriHelperTests/UriHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Toolbox.Common.Helpers;
+using Xunit;
+
+
+namespace Digipolis.Toolbox.Common.UnitTests.Helpers
+{
+    
+    public class IsValidUriTests
+    {
+        [Fact]
+        public void ValidUriReturnsTrue()
+        {
+            var uri = "http://www.mysite.local";
+            var ret = UriHelper.IsValidUri(uri);
+            Assert.True(ret);
+        }
+        
+        [Fact]
+        public void InvalidUriReturnsFalse()
+        {
+            var uri = "c:/www.mysite.local";
+            var ret = UriHelper.IsValidUri(uri);
+            Assert.False(ret);
+        }
+    }
+}
